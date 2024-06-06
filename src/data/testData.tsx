3e257bc2f59ba9926f6917fc719c83788966ad5d
billyfarrell95 <billyfarrell95@gmail.com>
2024-06-05 21:42:56 -0500

    Update testData.tsx

--- src/data/testData.tsx
+++ src/data/testData.tsx
@@ -3,54 +3,60 @@ const testData = [
       id: crypto.randomUUID(),
       date: "2024-05-23",
       vehicle: "Vehicle Three",
+      cost: "52.00",
       description: "Oil Change",
       shop: "Shop name 123",
-      mileage: "135000",
+      mileage: "135,000",
       memo: "Checked and topped off all fluids"
   },
   {
       id: crypto.randomUUID(),
       date: "2024-05-02",
       vehicle: "Vehicle One",
+      cost: "45.00",
       description: "Tire rotation",
       shop: "Shop name 123",
-      mileage: "134500",
+      mileage: "134,500",
       memo: "Front tires showing signs of wear"
   },
   {
       id: crypto.randomUUID(),
       date: "2023-05-02",
       vehicle: "Vehicle Two",
+      cost: "243.00",
       description: "New brakes",
       shop: "Shop name 123",
-      mileage: "110000",
+      mileage: "110,000",
       memo: "Brake pads and rotors replaced"
   },
   {
       id: crypto.randomUUID(),
       date: "2023-12-05",
       vehicle: "Vehicle One",
+      cost: "123.00",
       description: "Coolant flush",
       shop: "Shop name 789",
-      mileage: "115000",
+      mileage: "115,000",
       memo: "Coolant system inspected for leaks"
   },
   {
       id: crypto.randomUUID(),
       date: "2024-02-28",
       vehicle: "Vehicle Three",
+      cost: "40.12",
       description: "Air filter replacement",
       shop: "Shop name 456",
-      mileage: "131000",
+      mileage: "131,000",
       memo: "Air intake cleaned"
   },
   {
       id: crypto.randomUUID(),
       date: "2023-10-15",
       vehicle: "Vehicle Two",
+      cost: "1254.23",
       description: "Spark plug replacement",
       shop: "Shop name 789",
-      mileage: "112500",
+      mileage: "112,500",
       memo: "Performed engine tune-up"
   }
 ];
